1c9073d510c8fe1a92649a900b6bc1202859a6bc
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-04-16 14:20:46 +0200

    refactor: update recipe

--- demo/interfaces/Recipe.php
+++ demo/interfaces/Recipe.php
@@ -1,5 +1,21 @@
 <?php
 
-class Recipe {
-  
+class Recipe implements Crud, Actions {
+  function do() : void {
+    echo 'miam';
+  }
+  function create () :void {
+    echo 'ok';
+  }
+  function read() {
+    echo 'recipe';
+  }
+  function update(): void
+  {
+    echo 'ok';
+  }
+  function delete(): void
+  {
+    echo 'finished';
+  }
 }
